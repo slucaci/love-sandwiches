57b5512942df816cce1437757c74e9df62e5b64f
slucaci <serjlucaci@gmail.com>
2024-05-26 22:24:26 +0100

    Collect sales data for last 5 markets in list of columns

--- run.py
+++ run.py
@@ -80,6 +80,21 @@ def calculate_surplus(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales worksheet, collecting 
+    the last 5 entries for each sandwich and returns the data 
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        last_5_entries = column[-5:]
+        columns.append(last_5_entries)
+    return columns
+
 def main():
     """
     Run all program functions
@@ -92,4 +107,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
